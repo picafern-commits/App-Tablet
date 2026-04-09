3917376a1b984e85164e781254b509c04646ec11
picafern-commits <pica.fern@gmail.com>
2026-04-09 23:12:34 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,8 +1,7 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCSgw4rhBLW5mq4QClulubf6e0hf5lDJbo",
-  authDomain: "toner-manager-756c4.firebaseapp.com",
-  projectId: "toner-manager-756c4"
+  apiKey: "AQUI",
+  authDomain: "AQUI",
+  projectId: "AQUI"
 };
-
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
