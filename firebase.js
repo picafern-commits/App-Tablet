9cd13f97ca959b8a818b2f65cd334733d9b373b1
picafern-commits <pica.fern@gmail.com>
2026-04-09 23:01:05 +0100

    Update and rename sw.js to firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSgw4rhBLW5mq4QClulubf6e0hf5lDJbo",
+  authDomain: "toner-manager-756c4.firebaseapp.com",
+  projectId: "toner-manager-756c4"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
