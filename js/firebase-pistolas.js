5b914db10604bd12192b739a997efa82de54f2db
picafern-commits <pica.fern@gmail.com>
2026-05-13 18:53:31 +0100

    Add files via upload

---
+++ js/firebase-pistolas.js
@@ -0,0 +1,40 @@
+
+(function(){
+
+  try{
+
+    if(!window.db){
+      console.error("Firebase DB indisponível");
+      return;
+    }
+
+    window.db.collection("pistolas")
+    .onSnapshot((snapshot)=>{
+
+      window.pistolasData =
+        snapshot.docs.map(doc=>({
+          id: doc.id,
+          ...doc.data()
+        }));
+
+      console.log(
+        "Pistolas Firebase:",
+        window.pistolasData.length
+      );
+
+      if(typeof renderPistolas === "function"){
+        renderPistolas(window.pistolasData);
+      }
+
+    });
+
+  }catch(error){
+
+    console.error(
+      "Erro Firebase Pistolas:",
+      error
+    );
+
+  }
+
+})();
