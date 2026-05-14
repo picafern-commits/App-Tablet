af0c0941fbc3168664cf965cf7df351c4128d14e
picafern-commits <pica.fern@gmail.com>
2026-05-14 18:28:58 +0100

    Add files via upload

--- js/firebase-portas.js
+++ js/firebase-portas.js
@@ -1,40 +1,14 @@
+function esperarFirebase(callback) {
+    const check = setInterval(() => {
+        if (window.db) {
+            clearInterval(check);
+            callback(window.db);
+        }
+    }, 100);
+}
 
-(function(){
+esperarFirebase((db) => {
+    console.log("Firebase ligado");
 
-  try{
-
-    if(!window.db){
-      console.error("Firebase DB indisponível");
-      return;
-    }
-
-    window.db.collection("portas")
-    .onSnapshot((snapshot)=>{
-
-      window.portasData =
-        snapshot.docs.map(doc=>({
-          id: doc.id,
-          ...doc.data()
-        }));
-
-      console.log(
-        "Portas Firebase:",
-        window.portasData.length
-      );
-
-      if(typeof renderPortas === "function"){
-        renderPortas(window.portasData);
-      }
-
-    });
-
-  }catch(error){
-
-    console.error(
-      "Erro Firebase Portas:",
-      error
-    );
-
-  }
-
-})();
+    // COLOCA AQUI O onSnapshot / listeners
+});
