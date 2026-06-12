bffcfb8f8b7a1a1de2ee70194b56ca4d269992d0
picafern-commits <pica.fern@gmail.com>
2026-06-12 11:23:14 +0100

    Update

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ if (typeof firebase !== "undefined") {
   }
 }
 
-const APP_VERSION = "1.33.6";
+const APP_VERSION = "1.33.7";
 const APP_BRAGA_DEFAULT_VAPID_PUBLIC_KEY = "BE2xnhqmSPq85_kA6comGATxEseSoh8zY_EK_4NZsbiI1HJByjc1PgQqhTsUwPlr1ujuUSpSzp29AQeS1hnlHOQ";
 
 
