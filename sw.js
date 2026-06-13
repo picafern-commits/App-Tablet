00286711d8bb36f907c447a3dff2b1d23be951c9
picafern-commits <pica.fern@gmail.com>
2026-06-14 00:44:35 +0100

    23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_BRAGA_SW = "app-braga-runtime-v91";
+const APP_BRAGA_SW = "app-braga-runtime-v92";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -119,4 +119,4 @@ self.addEventListener("notificationclick", (event) => {
   );
 });
 
-// APP BRAGA V1.54.4 restore-old-sidebar
+// APP BRAGA V1.54.5 old-sidebar-fixed-scroll
